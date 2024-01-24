4f0bd27c3b4d9caeab9385682b30874f5629304e
Felipe Portari <felipe.portari7@gmail.com>
2024-01-24 10:25:44 -0300

    Adicionando blog novamente e removendo produtos.see

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,3 +9,15 @@
 <a href="{{ route('blog') }}">
     <button>Blog</button>
 </a>
+<a href="{{ route('product') }}">
+    <button>Produtos</button>
+</a>
+<a href="{{ route('customer') }}">
+    <button>Cliente</button>
+</a>
+<a href="{{ route('suppliers') }}">
+    <button>Fornecedores</button>
+</a>
+<a href="{{ route('login') }}">
+    <button>Login</button>
+</a>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SuppliersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,5 +28,9 @@ Route::get('/sobre', [AboutController::class, 'index'])->name('about');
 Route::get('/contato', [ContactController::class, 'index'])->name('contact');
 
 Route::get('/blog', [BlogController::class, 'index'])->name('blog');
-
 Route::get('/blog/{slug}', [BlogController::class, 'see'])->name('blog.see');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/clientes', [CustomerController::class, 'index'])->name('customer');
+Route::get('/fornecedores', [SuppliersController::class, 'index'])->name('suppliers');
+Route::get('/produtos', [ProductController::class, 'index'])->name('product');
