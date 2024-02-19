56331ea269a8f4665f101bd1ea881cda94bb4dae
Felipe Portari <felipe.portari7@gmail.com>
2024-02-19 16:05:11 -0300

    Alterando views conforme o professor do curso solicitou

---
+++ resources/views/other_pages/index.blade.php
@@ -0,0 +1,35 @@
+<h1>{{ $message }}</h1>
+
+<a href="{{ route('site.about') }}">
+    <button>Sobre Nós</button>
+</a>
+<a href="{{ route('site.contact') }}">
+    <button>Contato</button>
+</a>
+<a href="{{ route('site.blog') }}">
+    <button>Blog</button>
+</a>
+<a href="{{ route('admin.login') }}">
+    <button>Login</button>
+</a>
+<a href="{{ route('admin.product') }}">
+    <button>Produtos</button>
+</a>
+<a href="{{ route('admin.customer') }}">
+    <button>Cliente</button>
+</a>
+<a href="{{ route('admin.suppliers') }}">
+    <button>Fornecedores</button>
+</a>
+
+<h1>Páginas do Professor (enquanto não criei as minhas hehe)</h1>
+
+<a href="{{ route('admin.customer') }}">
+    <button>Cliente</button>
+</a>
+<a href="{{ route('admin.customer') }}">
+    <button>Cliente</button>
+</a>
+<a href="{{ route('admin.customer') }}">
+    <button>Cliente</button>
+</a>
